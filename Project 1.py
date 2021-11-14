06cb0152823ae27f34a835eba7c3352509d1a02b
MakinFantasy <beingp0z1t1v3@gmail.com>
2021-11-14 20:03:28 +0300

    Добавлена функция для создания папки на диске

--- Project 1.py
+++ Project 1.py
@@ -1,9 +1,12 @@
 import requests
 from pprint import pprint
+import datetime
 
 class VkUpload:
     def __init__(self):
         self.token = 'AQAAAAAZJUXrAADLW-qtmvkOBUvjvOye265SgRU'
+        date = datetime.datetime.now().strftime("%d-%b-%Y %H-%M-%S")
+        self.path = f'Backup_{date}'
 
     def get_photos(self):
         url = 'http://api.vk.com/method/photos.get'
@@ -16,10 +19,40 @@ class VkUpload:
             'count': '5'
         }
         response = requests.get(url, params=params)
+        data_vk = response.json()
+        pprint(data_vk)
+        return
+
+    def create_folder(self):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        headers = {
+            'Content_Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+        params = {
+            'path': 'netology/photos'
+        }
+        requests.put(url, headers=headers, params=params )
+
+
+    def upload_photos(self, file_path: str):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = {
+            'Content_Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+        params = {
+            'path': file_path,
+            'overwrite':'true'
+        }
+
+        response = requests.get(upload_url, headers=headers, params=params)
         data = response.json()
-        pprint (data)
+        pprint(data)
         return data
 
 
 
-get_photos()
+
+a = VkUpload()
+a.create_folder()
